2757cfa2c716f1bb4331cace3d023d72a5c2fce9
lucas <lcs.carvajal@gmail.com>
2025-12-16 07:50:56 +0100

    refactor and create Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
 use std::env;
+use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching in {file_path} for {query}.");
+    println!(
+        "Searching in '{}' for '{}'.",
+        config.file_path, config.query
+    );
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
